027e7fb09e56a07f0f28e4614446e0526f26c878
uriahadamsmith <uriahadamsmith@gmail.com>
2025-05-17 17:56:50 -0400

    Update server.js for Glitch file structure

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the app directory
-app.use(express.static(path.join(__dirname, 'app')));
+// Serve static files from the root directory
+app.use(express.static('.'));
 
 // Serve index.html for all routes (SPA support)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'app', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start server
